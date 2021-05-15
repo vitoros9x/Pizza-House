50315362a76c686d83d8d9baa4cab9789fa5f09c
vitoros9x <tutv@hamsa.co>
2021-05-15 10:27:50 +0700

    update silder, creat welcome sectiona and box image

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,23 @@
-var cartModal = document.querySelector('.header-cart__modal')
-document.querySelector('.header-btn__icon-cart').onclick = () => {
-  cartModal.classList.toggle('modal-visible')
-}
+var iconBtn = document.querySelectorAll('.header-btn__icon');
+[...iconBtn].forEach((item) => {
+  item.onclick = () => {
+    if (!item.classList.contains('modal-visible')) {
+      [...iconBtn].forEach((item) => {
+        item.querySelector('.header-modal').classList.remove('modal-visible')
+      });
+      item.querySelector('.header-modal').classList.add('modal-visible');
+    } else {
+      item.querySelector('.header-modal').classList.remove('modal-visible')
+    }
+  }
+})
+
+$(document).ready(() => {
+  $('.banner-slider').slick(
+    {
+      arrows: false,
+      dots: true,
+      autoplay: true
+    }
+  );
+});
